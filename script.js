9b12b80cfaaf4720a7389640aabf39fde5d89489
Jad Bizri <91910175+JadBizri@users.noreply.github.com>
2023-07-23 16:53:37 -0400

    Apply button functionality to choose grid size

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-function drawGrid() {
-    const divs = 4;
-    let width = (400 / divs) / 400 * 100;
+function drawGrid(divs) {
+    // Clear the existing grid
+    const container = document.querySelector(".container");
+    container.innerHTML = '';
+
+    let width = (400 / divs) / 400 * 100; //to hold width of each div container
+
+    //adjust width and height as needed
     const styleSheet = document.styleSheets[0];
     styleSheet.insertRule(`.blocks { width: ${width}%; }`, styleSheet.cssRules.length);
     styleSheet.insertRule(`.blocks { height: ${width}%; }`, styleSheet.cssRules.length);
-    const container = document.querySelector(".container");
+
+    //for loop to add all divs and create a new grid
     for (let i = 0; i < divs * divs; i++) {
         let div = document.createElement("div");
         div.classList.add("blocks");
@@ -12,13 +18,19 @@ function drawGrid() {
     }
 }
 
-drawGrid();
+drawGrid(16);
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("size");
+let slider = document.getElementById("myRange");
+let output = document.getElementById("size"); //to show current grid size
 output.innerHTML = `${slider.value}x${slider.value}`; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   output.innerHTML = `${this.value}x${this.value}`;
-} 
+}
+
+//create a new grid with selected size
+let applyButton = document.getElementById("apply-button");
+applyButton.onclick = function(){
+    drawGrid(slider.value);
+}
