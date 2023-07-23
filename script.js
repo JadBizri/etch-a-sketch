de521143bfd57448a81f62e8647b6880e22c43e2
Jad Bizri <91910175+JadBizri@users.noreply.github.com>
2023-07-23 18:24:32 -0400

    Add color changer

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const DEFAULT_SIZE = 16;
+const DEFAULT_COLOR = '#000000';
+const DEFAULT_MODE = 'color';
+
+let currentColor = DEFAULT_COLOR
+let currentMode = DEFAULT_MODE
+let currentSize = DEFAULT_SIZE
+
+//function to set new color
+function setCurrentColor(newColor) {
+    currentColor = newColor
+}
+
+//function to set new color
+function setCurrentMode(newMode) {
+    activateButton(newMode)
+    currentMode = newMode
+}
+
+//function to set new size
+function setCurrentSize(newSize) {
+    currentSize = newSize
+}
+
 const container = document.querySelector(".container"); //grid container
 
 function drawGrid(divs = 16) {
@@ -22,13 +46,13 @@ let output = document.getElementById("size"); //to show current grid size
 output.innerHTML = `${slider.value}x${slider.value}`; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = `${this.value}x${this.value}`;
+slider.oninput = function () {
+    output.innerHTML = `${this.value}x${this.value}`;
 }
 
 //create a new grid with selected size
 let applyButton = document.getElementById("apply-button");
-applyButton.onclick = function(){
+applyButton.onclick = function () {
     drawGrid(slider.value);
 }
 
